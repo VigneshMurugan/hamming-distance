7ac3fe3309848b36ec05fba8182046fd00967251
Vignesh Murugan <vignesh328@gmail.com>
2021-01-17 13:31:36 +0530

    Hamming distance calculator for String and Number

---
+++ index.js
@@ -0,0 +1,42 @@
+var assert = require('assert')
+
+function hammingDistance(firstValue, secondValue) {
+    let dataType = typeof firstValue;
+
+    switch(dataType) {
+        case 'string':
+            return distanceBetweenStrings(firstValue, secondValue);
+        case 'number':
+            return distanceBetweenNumbers(firstValue, secondValue)
+        default:
+            throw new Error('Type not supported')
+    }
+}
+
+function distanceBetweenStrings(firstValue, secondValue) {
+    assert.strictEqual(firstValue.length, secondValue.length, 'String should be of equal length')
+    return [...firstValue].reduce((acc, current, index) => {
+        if(current != secondValue[index])
+            acc++
+        return acc
+    }, 0)
+}
+
+function distanceBetweenNumbers(firstNumber, secondNumber) {
+    var x = firstNumber ^ secondNumber; 
+    var setBits = 0; 
+  
+    while (x > 0)  
+    { 
+        setBits += x & 1; 
+        x >>= 1; 
+    } 
+  
+    return setBits; 
+}
+
+module.exports = hammingDistance;
+
+
+
+console.log(hammingDistance('dasds','dasfadgagd'))
